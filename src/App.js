c68709a0bea4bad3ed9ee518abe838bc6b8808b5
Pedro Ferrari <pedro@pedroferrari.com>
2018-07-12 10:51:55 +0200

    My first reducer & action

--- src/App.js
+++ src/App.js
@@ -21,6 +21,27 @@ class App extends Component {
 
 export default App;
 
-const hello = () => ({ welcome: 'hello' });
-const store = createStore(hello);
+const defaultState = {
+	welcome: 'Hi',
+	otherState: 'some stuff',
+	otherStates: 'some other stuff'
+};
+const greeting = (state = defaultState, action) => {
+	switch (action.type) {
+		case 'GREET_ME':
+			return { ...state, welcome: 'Hello Pedro' };
+		case 'GREET_WORLD':
+			return { ...state, welcome: 'Hello World' };
+		default:
+			return state;
+	}
+};
+const store = createStore(greeting);
+
+console.log(store.getState());
+
+store.dispatch({
+	type: 'GREET_ME'
+});
+
 console.log(store.getState());
